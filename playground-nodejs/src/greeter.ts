77665270385285ef1518e9e6d63df64c6ef4a807
oktntko <oktntko@gmail.com>
2022-05-11 22:25:58 +0900

    ✨ [nodejs] コメントアウト

--- playground-nodejs/src/greeter.ts
+++ playground-nodejs/src/greeter.ts
@@ -1,5 +1,15 @@
-const main = async () => {
-  console.log("Hello World");
+/**
+ * JSDoc
+ *
+ * @param {string[]} args 引数
+ */
+const main = async (args: string[] = []) => {
+  console.log("Hello World"); // １行コメント
+  const i /*文中コメント*/ = 1;
+  /*
+   * 改行コメント
+   */
+  console.log(args);
 };
 
 main();
