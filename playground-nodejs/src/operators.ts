720be84c0dbf08d1008777e84bc6171bc911886d
oktntko <oktntko@gmail.com>
2022-05-11 22:52:13 +0900

    ✨ [nodejs] 四則演算

---
+++ playground-nodejs/src/operators.ts
@@ -0,0 +1,9 @@
+export const main = () => {
+  const a1 = 5 + 7 - 6;
+  const a2 = 720 / 30;
+  const a3 = 11 * 2 + 12 / 4;
+  const a4 = (18 / 6) * 3;
+  console.log(a1, a2, a3, a4);
+};
+
+main();
